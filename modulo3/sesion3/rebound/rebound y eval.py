bd1c7caec10a82cf9623ddb5f1c6d54e1a95e796
nataliarw <natalia.romanini@gmail.com>
2023-04-19 20:02:54 -0500

    comenzando con python

---
+++ modulo3/sesion3/rebound/rebound y eval.py
@@ -0,0 +1,25 @@
+#declaro variables
+a = 20
+b = 30
+# realizo la operacion
+a*b
+# imprimo resultado
+print (a*b)
+
+# o bien
+#declaro variables
+a = 20
+b = 30
+# imprimo resultado
+print (a*b)
+
+
+Y=81
+from math import sqrt
+c = sqrt(81)
+print (c)
+
+Y=81
+import math
+c = sqrt(81)
+print (c)
